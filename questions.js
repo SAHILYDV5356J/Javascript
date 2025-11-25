ada6ec978a5ab0138e516482b9a4aff181e99208
Sahil Yadav <sy05092005@gmail.com>
2025-11-25 15:46:11 +0530

    change7

--- questions.js
+++ questions.js
@@ -61,3 +61,15 @@ for(let details in student){
 
 
 
+//checking multiple of 5
+
+var num= prompt("enter a number: ");
+
+
+if((num%5)===0){
+    console.log(`${num} is multiple of 5`);
+}
+else{
+    console.log(`${num} is not a multiple of 5`);
+}
+
