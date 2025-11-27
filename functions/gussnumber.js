5ff481fa4c39523bb1dd04b4877bc13593af0111
Sahil Yadav <sy05092005@gmail.com>
2025-11-27 23:18:08 +0530

    making directory and file in it

---
+++ functions/gussnumber.js
@@ -0,0 +1,24 @@
+function guess(rdm){
+
+     let input=prompt("enter a number {0-10}");
+     while(input!=rdm){
+          
+          if(Number(input)<rdm){
+            console.log("your number is too less: ");
+          }
+          else if(Number(input)>rdm){
+            console.log("your number is too greater: ");
+          }
+
+          input=prompt("try again :");
+     
+     }
+     console.log(`correct guess`);
+   
+}
+
+let random=Math.random();
+let rdm= Math.floor(random*100+1);
+
+
+guess(rdm);
